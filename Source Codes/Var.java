4058141e82b1dadb2edb76b48fda9f425eaf271f
Ifaldzi <54470264+Ifaldzi@users.noreply.github.com>
2020-10-08 09:00:38 +0700

    Add files via upload

---
+++ Source Codes/Var.java
@@ -0,0 +1,11 @@
+public class Var
+{
+	public static void main(String[] args)
+	{
+		int exampleVar;
+		exampleVar = 990;
+		System.out.println(exampleVar);
+		double anotherVar = 890.3434;
+		System.out.println(anotherVar);
+	}
+}
