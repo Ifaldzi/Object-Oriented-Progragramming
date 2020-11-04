629b37057941f5ff5e16c58ec0dff5b1c29e12ae
Ifaldzi <ifaldzialwihudhori@gmail.com>
2020-11-04 17:37:34 +0700

    Codes for Kasus Week 6 PBO JTK

---
+++ Source Codes/Kasus/Week6/Circle.java
@@ -0,0 +1,49 @@
+public class Circle
+{
+	private double radius;
+	private String color;
+
+	public Circle()
+	{
+		radius = 1.0;
+		color = "red";
+	}
+
+	public Circle(double r)
+	{
+		radius = r;
+		color = "red";
+	}
+
+	/** 3rd constructor with argument: radius, color */
+	public Circle(double r, String c)
+	{
+		radius = r;
+		color = c;
+	}
+
+	public String getColor()
+	{
+		return color;
+	}
+
+	public double getRadius()
+	{
+		return radius;
+	}
+
+	public void setColor(String c)
+	{
+		color = c;
+	}
+
+	public double getArea()
+	{
+		return radius*radius*Math.PI;
+	}
+
+	public String toString()
+	{
+		return "Circle[radius=" + radius + " color=" + color + "]";
+	}
+}
---
+++ Source Codes/Kasus/Week6/Sortable.java
@@ -0,0 +1,21 @@
+abstract class Sortable
+{
+	public abstract int compare(Sortable b);
+	public static void shell_sort(Sortable[] a)
+	{
+		//shell sort body
+		for(int interval=a.length/2; interval>0; interval/=2)
+		{
+			for(int i=interval; i<a.length; i++)
+			{
+				Sortable temp = a[i];
+				int j;
+				for(j=i; j>=interval && a[j-interval].compare(temp)==-1; j-=interval)
+				{
+					a[j] = a[j-interval];
+				}
+				a[j] = temp;
+			}	
+		}
+	}
+}
